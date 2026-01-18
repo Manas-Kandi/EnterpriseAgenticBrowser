d4ad001525ec65fb3cc736a5d8e612fde2c9273b
Manas-Kandi <manaskandimalla2002@gmail.com>
2026-01-17 18:12:41 -0600

    chore: update production bundle hash reference in main entry point

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -3,7 +3,7 @@ import "node:url";
 import "node:path";
 import "node:crypto";
 import "node:fs/promises";
-import { M, R, V } from "./main-CEh3nkAd.js";
+import { M, R, V } from "./main-CjJ1UnIZ.js";
 export {
   M as MAIN_DIST,
   R as RENDERER_DIST,
